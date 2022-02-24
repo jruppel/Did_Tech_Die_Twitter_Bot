94388fe2dea5f87f7a892c787510be49394f831e
Jonathan Ruppel <jonathan.ruppel@yahoo.com>
2022-02-24 17:12:01 -0600

    tweaks to games/weeks played + bye weeks

--- did_tech_die_cfb.py
+++ did_tech_die_cfb.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import calendar
+from dataclasses import dataclass
 import re
 import time
 from turtle import bye
@@ -27,31 +28,44 @@ def get_calendar_data():
     except ApiException as e:
         print("Exception when calling GamesApi->get_calendar: %s\n" % e)
 
-#Todo: if cyrrent_date is after end_data, return the last calendar week
 def get_game_week(calendar_data):
-    for i in range(len(calendar_data)-1):
+    #Iterate thru year calendar
+    for i in range(len(calendar_data)):
+        #Assign the month, day, year of week's start and end date 
         start_date = time.strptime(calendar_data[i].first_game_start[:10], "%Y-%m-%d")
-        current_date = time.strptime("2019-12-25", "%Y-%m-%d")
-        end_date = time.strptime(calendar_data[i+1].first_game_start[:10], "%Y-%m-%d")
-        if current_date >= start_date and current_date < end_date:
+        current_date = time.strptime("2019-12-20", "%Y-%m-%d") #testing
+        end_date = time.strptime(calendar_data[i].last_game_start[:10], "%Y-%m-%d")
+        #
+        if current_date >= start_date and current_date <= end_date:
             week = calendar_data[i].week
             season_type = calendar_data[i].season_type
             return week, season_type
-
+        
 def get_total_games_played():
     try:
         # Team records
-        api_response = api_instance.get_team_records(year=2019, team=team)
+        api_response = api_instance.get_team_records(year=2019, team=team) #testing
         record_data = api_response[0]
         total_games = record_data.total["games"]
         return total_games
     except ApiException as e:
         print("Exception when calling GamesApi->get_team_records: %s\n" % e)
 
+def get_total_weeks_played():
+    game_week = get_game_week(get_calendar_data())
+    total_weeks = games_played = get_total_games_played()
+    bye_weeks = get_bye_weeks()
+    for i in bye_weeks:
+        if total_weeks >= i:
+            total_weeks = total_weeks + 1
+    if game_week[1] == 'postseason':
+        total_weeks = total_weeks + 1
+    return total_weeks
+
 def get_game_data(week, season_type):
     try:
         # Games and results
-        api_response = api_instance.get_games(year=2019, week=week, season_type=season_type, team=team)
+        api_response = api_instance.get_games(year=2019, week=week, season_type=season_type, team=team) #testing
         game_data = api_response[0]
         return game_data
     except ApiException as e:
@@ -60,28 +74,25 @@ def get_game_data(week, season_type):
         pass
         #print("Exception when calling CFBGamesApi->get_games: %s\n" % e)
 
+#Determine how many bye weeks have happened up to the total games played
 def get_bye_weeks():
+    #Iterate thru total games played, game data starts at week 1
     for i in range(1, get_total_games_played()+1):
+        #Bye weeks only happen during the regaular season
         week_data = get_game_data(i, "regular")
+        #Each week in which game data does not exist is a bye
         if week_data == None:
+            #Store previous week's number and add 1
             all_bye_weeks.append(get_game_data(i-1, "regular").week+1)
     return all_bye_weeks
 
 def game_is_final():
-    final = False
-    #Todo: if post-season, add post-season week(s) to game week
-    game_week = get_game_week(get_calendar_data())
-    games_played = get_total_games_played()
-    bye_weeks = get_bye_weeks()
-    for i in bye_weeks:
-        if games_played >= i:
-            games_played = games_played + 1
-    if game_week == games_played:
-        final = True
-    #Todo: increment games_played for post-season played
-    #print(games_played)
+    #final = False
+    #if game_week == games_played:
+    final = True
+    #print(total_weeks)
     #print(game_week)
-    return final
+    #return final
 
 def get_result(game_data):
     if game_data.home_team == "Louisiana Tech":
@@ -96,4 +107,7 @@ def get_result(game_data):
             result = 'L'
     return result
 
-#print(game_is_final())
+#print(get_game_week(get_calendar_data()))
+#game_is_final()
+#print(get_result(get_game_data(1,"postseason")))
+print(get_total_weeks_played())
