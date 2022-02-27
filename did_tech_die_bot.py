6cd94d1bed10e281b9e2a444a8da5018016fd9e6
Jonathan Ruppel <jonathan.ruppel@yahoo.com>
2022-02-27 16:15:36 -0600

    added is_today_gameday

--- did_tech_die_bot.py
+++ did_tech_die_bot.py
@@ -12,15 +12,14 @@ access_token=access_token,access_token_secret=access_token_secret)
 
 cfb_game_week = did_tech_die_cfb.get_game_week()
 cfb_game_data = did_tech_die_cfb.get_game_data(cfb_game_week[0], cfb_game_week[1])
-cfb_gameday = did_tech_die_cfb.today_is_football()
-cfb_tweets = cfb_game_week[0] - 1
+cfb_is_today_gameday = did_tech_die_cfb.is_today_gameday()
+#cfb_tweets = cfb_game_week[0] - 1
 
 def create_cfb_tweet():
     # Get CFB Game data
     print("Checking if game is final.")
     cfb_game_is_final = did_tech_die_cfb.game_is_final()
-    tweets = cfb_tweets
-    if cfb_game_is_final and cfb_gameday and tweets != cfb_game_week[0]:
+    if cfb_game_is_final and cfb_is_today_gameday:
         cfb_game_result = did_tech_die_cfb.get_result(cfb_game_data)
         print("Crafting tweet.")
         #Create tweet
@@ -32,8 +31,6 @@ def create_cfb_tweet():
         cfb_tweets = cfb_tweets + 1
     print(response)
 
-print(cfb_tweets)
-create_cfb_tweet()
-print(cfb_tweets)
+#create_cfb_tweet()
 #Todo: check if cfb game is final on game days after some time interval 
 #Todo: when game is final: tweet and stop checking if game is final until the next game week
