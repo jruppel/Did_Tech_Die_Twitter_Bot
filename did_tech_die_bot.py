34aa5bf0f16a21d33cc1bfae2ad9f80c7acdb19d
Jonathan Ruppel <jonathan.ruppel@yahoo.com>
2022-03-01 21:40:34 -0600

    create tweet tweaks

--- did_tech_die_bot.py
+++ did_tech_die_bot.py
@@ -36,10 +36,15 @@ def create_cfb_tweet():
                 #Create tweet
                 #Todo: include sport, teams, and score in tweet
                 if cfb_game_result == 'L':
-                    response = client.create_tweet(text='Yes, ' + home_team + ' ' + home_pts + ' - ' + away_team + ' ' + away_pts)
+                    if home_team == did_tech_die_cfb.team:
+                        response = client.create_tweet(text='Yes\n' + '🏈 Tech ' + home_pts + ' - ' + away_team + ' ' + away_pts)
+                    else:
+                        response = client.create_tweet(text='No\n' + '🏈 Tech ' + away_pts + ' - ' + home_team + ' ' + home_pts)
                 if cfb_game_result == 'W':
-                    print(cfb_game_data)
-                    response = client.create_tweet(text='No, ' + home_team + ' ' + home_pts + ' - ' + away_team + ' ' + away_pts)
+                    if home_team == did_tech_die_cfb.team:
+                        response = client.create_tweet(text='No\n' + '🏈 Tech ' + home_pts + ' - ' + away_team + ' ' + away_pts)
+                    else:
+                        response = client.create_tweet(text='Yes\n' + '🏈 Tech ' + away_pts + ' - ' + home_team + ' ' + home_pts)
                 print(response)
 
 create_cfb_tweet()
