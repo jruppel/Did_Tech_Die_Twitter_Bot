96b8c99784af2577e73e25970cb83d2b30a810a1
Jonathan Ruppel <jonathan.ruppel@yahoo.com>
2023-05-24 16:08:16 -0500

    boxscore record fix

--- web_scraping.py
+++ web_scraping.py
@@ -6,7 +6,7 @@ import urllib.request
 from bs4 import BeautifulSoup
 import constants
 
-year,last_year,next_year,today,yesterday_date,current_date,url,team,team_abbr,logging=constants.year,constants.last_year,constants.next_year,constants.today,constants.yesterday_date,constants.current_date,constants.url,constants.team,constants.team_abbr,constants.logging
+year,last_year,next_year,today,yesterday_date,current_date,url,team,team_abbr,boxscore_split,logging=constants.year,constants.last_year,constants.next_year,constants.today,constants.yesterday_date,constants.current_date,constants.url,constants.team,constants.team_abbr,constants.boxscore_split,constants.logging
 
 def get_sport_url(sport):
     if sport in {'mens-basketball','womens-basketball','womens-tennis','womens-bowling','mens-golf'}:
@@ -22,9 +22,9 @@ def get_sport_url(sport):
     sport_url='{}/sports/{}/schedule/{}?grid=true'.format(url,sport,url_year)
     return sport_url
 
-def get_website_data(url, sport):
+def get_website_data(url,sport):
     try:
-        df=pd.read_html(url, header=0)[0]
+        df=pd.read_html(url,header=0)[0]
         recent_games=df[df.Date.isin([current_date,yesterday_date])].where(pd.notnull(df),None)
     except AttributeError:
         logging.warning("Current year schedule for this sport has not been created yet!")
@@ -34,32 +34,38 @@ def get_website_data(url, sport):
             logging.info("Tech did not play recently in this sport!")
             return   
         tech_games=recent_games[~recent_games.Opponent.str.contains("vs.")]
-        game_info=tech_games[['Date', 'Time', 'Opponent', 'At', 'Result']]
-        game_info.insert(0, 'Sport', sport.capitalize())
+        game_info=tech_games[['Date','Time','Opponent','At','Result']]
+        game_info.insert(0,'Sport',sport.capitalize())
         games=game_info.values.tolist()
         logging.info("Tech played recently in this sport!")
         logging.debug(games)
         return games
-    
-def get_boxscore_records(sport_url,opponent):
+
+def get_boxscore_records(sport_url):
     sport_page=urllib.request.urlopen(sport_url)
     sport_soup=BeautifulSoup(sport_page,"html.parser")
     # Retrieve the last a tag which has the text Box Score's href attribute value
     href_link=sport_soup.find_all('a',text='Box Score')[-1]['href']
-    boxscore_page = urllib.request.urlopen("{}{}".format(url,href_link))
-    boxscore_soup = BeautifulSoup(boxscore_page,"html.parser")
-    # Retrieve the text for the h2 tag with the team names and records
+    boxscore_page=urllib.request.urlopen("{}{}".format(url,href_link))
+    boxscore_soup=BeautifulSoup(boxscore_page,"html.parser")
+    # Retrieve the boxscore text for the h2 tag with the team names and records
     try:
-        boxscore_matchup = boxscore_soup.find('h2',{'class':'hide text-center text-uppercase hide-on-medium-down'}).text
+        boxscore_matchup=boxscore_soup.find('h2',{'class':'hide text-center text-uppercase hide-on-medium-down'}).text
     except AttributeError:
-        logging.warning("Matchup not found on boxscore page! No records will be added to tweet!")
-        return "",""
+        try:
+            boxscore_matchup = boxscore_soup.find('h2',{'class':'hide'}).text
+        except AttributeError:
+            logging.warning("Matchup not found on boxscore page! No records will be added to tweet!")
+            return "",""
     logging.info("Boxscore matchup: {}".format(boxscore_matchup))
-    try:
-        boxscore_team_split = boxscore_matchup.split(team)[1].lstrip()
-    except IndexError:
-        boxscore_team_split = boxscore_matchup.split(team_abbr)[1].lstrip()
-    boxscore_team_record = re.findall(f'[^{")"}]+\)?', boxscore_team_split)[0]
-    boxscore_opponent_split = boxscore_matchup.split(opponent)[1].lstrip()
-    boxscore_opponent_record = re.findall(f'[^{")"}]+\)?', boxscore_opponent_split)[0]
+    # Retrieve team order from boxscore and split according
+    if boxscore_matchup.index(team)==0 or boxscore_matchup.index(team_abbr)==0:
+        boxscore_team_split=boxscore_matchup.split(boxscore_split)[0]
+        boxscore_opponent_split=boxscore_matchup.split(boxscore_split)[1].lstrip()
+    else:    
+        boxscore_team_split=boxscore_matchup.split(boxscore_split)[1].lstrip()
+        boxscore_opponent_split=boxscore_matchup.split(boxscore_split)[0]
+    # Regex search for '(' and ')' which inidicate the records
+    boxscore_team_record=re.search(r'(\(.*?\))',boxscore_team_split).group(1)
+    boxscore_opponent_record=re.search(r'(\(.*?\))',boxscore_opponent_split).group(1)
     return boxscore_team_record,boxscore_opponent_record
