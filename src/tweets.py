0308ae28f161df7ac477a975e71940d45c6713b8
Jonathan Ruppel <jonathan.ruppel@yahoo.com>
2023-09-27 13:55:56 -0500

    adding get tweet values

--- src/tweets.py
+++ src/tweets.py
@@ -9,21 +9,20 @@ import manage_db
 logging,client,delay,team,season,winter_sports,spring_sports,summer_sports,autumn_sports,boxscore_sports=constants.logging,constants.client,constants.delay,constants.team,constants.season,constants.winter_sports,constants.spring_sports,constants.summer_sports,constants.autumn_sports,constants.boxscore_sports
 
 def manage_tweets(sport):
+    sport_url=web_scraping.get_sport_url(sport)
     logging.info("Checking for recent {} games...".format(sport))
-    url=web_scraping.get_sport_url(sport)
-    games=web_scraping.get_website_data(url,sport)
+    games=web_scraping.get_website_data(sport_url,sport)
     if games is None:
-        return
+        return None
     game_num=len(games)
     for game in range(game_num):
         delay
-        sport,date,time,opponent,home_away,result,team_record,opponent_record,incorrect_tweet_id=games[game][0],games[game][1],games[game][2],games[game][3],games[game][4],games[game][5],None,None,None
-        time,game_is_exhibiton,game_is_final,is_duplicate=game_info.nan_time_to_time(time),game_info.is_game_exhibition(opponent),game_info.is_game_final(result),manage_db.is_game_in_db(sport,date,time,opponent,home_away,result)
+        team_sport,date,time,opponent,home_away,result,team_record,opponent_record,incorrect_tweet_id=game_info.get_team_sport(games[game][0]),games[game][1],game_info.nan_time_to_time(games[game][2]),games[game][3],games[game][4],games[game][5],None,None,None
+        game_is_exhibiton,game_is_final,is_duplicate=game_info.is_game_exhibition(opponent),game_info.is_game_final(result),manage_db.is_game_in_db(sport,date,time,opponent,home_away,result)
         if game_is_exhibiton or not game_is_final or is_duplicate:
-            return
-        if sport in boxscore_sports:
-            team_record,opponent_record=web_scraping.get_boxscore_records(url,game_num)
-        new_tweet=set_tweet(sport,opponent,result,team_record,opponent_record)
+            return None
+        win_loss,team_score,opponent_score,separator,reg_notes,add_notes,team_record,opponent_record=get_tweet_values(sport,sport_url,game_num,result)
+        new_tweet=set_tweet(team_sport,opponent,win_loss,team_score,opponent_score,separator,reg_notes,add_notes,team_record,opponent_record)
         response=client.create_tweet(text=new_tweet)
         new_tweet_id=response.data['id']
         tweet_url=f"https://twitter.com/user/status/{new_tweet_id}"
@@ -44,7 +43,15 @@ def manage_tweets(sport):
             sport,date,time,opponent,home_away,result,incorrect_tweet_id
             )
         game_num-=1
-            
+
+def get_tweet_values(sport,sport_url,game_num,result):
+    win_loss,team_score,opponent_score,reg_notes,add_notes=game_info.result_to_score(sport,result)
+    separator=get_separator(win_loss)
+    if sport in boxscore_sports:
+        team_record,opponent_record=web_scraping.get_boxscore_records(sport_url,game_num)
+    else:
+        team_record=opponent_record=None
+    return win_loss,team_score,opponent_score,separator,reg_notes,add_notes,team_record,opponent_record
 
 def delete_incorrect_tweet(tweet_id):
     client.delete_tweet(tweet_id)
@@ -58,10 +65,7 @@ def get_separator(win_loss):
         separator="finished"
     return separator
 
-def set_tweet(sport,opponent,result,team_record,opponent_record):
-    team_sport=game_info.get_team_sport(sport)
-    win_loss,team_score,opponent_score,reg_notes,add_notes=game_info.result_to_score(sport,result)
-    separator=get_separator(win_loss)
+def set_tweet(team_sport,opponent,win_loss,team_score,opponent_score,separator,reg_notes,add_notes,team_record,opponent_record):
     if [x for x in (win_loss,team_record,opponent_record) if x is not None]:
         if win_loss=='W' or win_loss=='T':
             pos_neg,win_team_record,win_team,lose_team_record,lose_team,win_score,lose_score="No.",team_record,team,opponent_record,opponent,team_score,opponent_score
